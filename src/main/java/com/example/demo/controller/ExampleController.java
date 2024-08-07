420167081abfcbdd87e8110486446b0aae1edc3e
hwicode <khc3796@naver.com>
2024-08-07 10:22:26 +0900

    logback.xml 파일 및 로그를 찍기 위한 ExampleController 생성

---
+++ src/main/java/com/example/demo/controller/ExampleController.java
@@ -0,0 +1,24 @@
+package com.example.demo.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ExampleController {
+
+    @GetMapping("/")
+    @ResponseStatus(value = HttpStatus.OK)
+    public String getExample() {
+        log.info("예제 메시지 : {}", "message");
+        return "ok";
+    }
+
+    @GetMapping("/e")
+    public String causeError() {
+        throw new IllegalStateException(new IllegalArgumentException(new RuntimeException("컨트롤러에서 에러가 발생함")));
+    }
+}
