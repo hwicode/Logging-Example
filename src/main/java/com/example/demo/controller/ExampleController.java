4c45ff18bc7112292b3dd758c781b9c21b4dfa2d
hwicode <khc3796@naver.com>
2024-08-08 23:49:17 +0900

    컨트롤러에서 예외처리를 진행해서 예외 발생 시에도 traceId, spanId가 찍히도록 수정

--- src/main/java/com/example/demo/controller/ExampleController.java
+++ src/main/java/com/example/demo/controller/ExampleController.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,5 +23,12 @@ public class ExampleController {
     public String causeError() {
         log.info("예제 메시지 : {}", "message");
         throw new IllegalStateException(new IllegalArgumentException(new RuntimeException("컨트롤러에서 에러가 발생함")));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        log.warn("예외 발생함 : ", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ex.getMessage());
     }
 }
